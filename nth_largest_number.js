2767076b8ef3d9c0d416d11ee6d5af7491c3d691
Shanmugapriya-numentica <shanmugapriya.murugan@numentica-ui.com>
2025-07-27 23:50:36 +0530

    Finding Largest Number

---
+++ nth_largest_number.js
@@ -0,0 +1,30 @@
+
+function findNthLargest(userInputArray, NthLargest){
+
+    if( !Array.isArray(userInputArray) || typeof NthLargest !=='number' || 
+        arguments.length !== 2 || userInputArray.length < NthLargest){
+            return "Entered Input is Invalid" ;
+        }
+
+    let numberOfElements = userInputArray.length ;
+    let newArray = userInputArray ;
+
+    for ( let i = 0 ; i < numberOfElements - 1 ; i++ ){
+        for ( let j =0 ; j <numberOfElements - 1 -i ; j++ ){
+            if (newArray[j] < newArray [j + 1]){
+
+                let temprorayHolder = newArray[j] ;
+                newArray[j] = newArray[ j + 1] ;
+                newArray[ j+1 ] = temprorayHolder ;
+            }
+        }
+    }
+    return NthLargest+ "The Number is: " + newArray[NthLargest - 1] ;
+}
+
+// console.log(findNthLargest([10,30,50,90],3))
+// console.log(findNthLargest(null,9))
+// console.log(findNthLargest([23,34,12],1,2))
+// console.log(findNthLargest("hello","good morning"))
+// console.log(findNthLargest(undefined,23))
+// console.log(findNthLargest(1233332,9))
